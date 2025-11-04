dade5345f3fd14798232bb2b10604bd861dc0cec
Pallavi Govind Rao <govindraopallavi@gmail.com>
2025-11-04 16:41:57 +0530

    Add files via upload

---
+++ input12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class input12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //int
+        // System.out.println("Input Your Rollno : ");
+        // int rollno = sc.nextInt();
+        // System.out.println(rollno);
+
+        //float
+        // System.out.println("Input Your Age : ");
+        // float age = sc.nextFloat();
+        // System.out.println(age);
+
+        //string
+        System.out.println("Input Your Name : ");
+        String name = sc.nextLine(); //next se 1 word print hoga
+        System.out.println(name);
+    }
+}
---
+++ mathsClass11.java
@@ -0,0 +1,13 @@
+public class mathsClass11 {
+    public static void main(String[] args) {
+        //max
+        System.out.println(Math.max(5,6));
+
+        //min
+        System.out.println(Math.min(5,6));
+
+        //for random value -> it give random long value
+        System.out.println((int)(Math.random()*100)); //typecaste kr rhe h int me aur do digit tk print kr rhe h
+
+    }
+}
